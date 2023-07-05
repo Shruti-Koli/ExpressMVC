2e9bb59965b28e4b8090d91def223deb663013f2
Shruti-Koli <koli.shruti55@gmail.com>
2023-07-05 13:58:08 +0530

    models

--- mod/app.js
+++ mod/app.js
@@ -1,14 +1,14 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
